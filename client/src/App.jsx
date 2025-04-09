c34002075c37da00d1a8b57cf8e4159105cfc5ad
Saleelvk <saleelvkshemi@gmail.com>
2025-04-09 20:20:51 +0530

    m

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -69,7 +69,7 @@ import Coupon from "./page/admin/pages/coupon/Coupon";
 import CreateCoupon from "./page/admin/pages/coupon/CreateCoupon";
 import EditCoupon from "./page/admin/pages/coupon/EditCoupon";
 import FindCoupons from "./page/user/profileDashboard/pages/findCoupons";
-import OrderConfirmation from "./page/user/components/OrderConfirmation";
+// import OrderConfirmation from "./page/user/components/OrderConfirmation";
 import SettingsPage from "./page/user/profileDashboard/pages/settings";
 import About from "./page/user/others/About";
 import Home from "./page/user/others/Home";
