47d03e0889e8b4b7ba5c7d853286e068d14cfb56
Saleelvk <saleelvkshemi@gmail.com>
2025-04-09 20:14:16 +0530

    m

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -69,7 +69,7 @@ import Coupon from "./page/admin/pages/coupon/Coupon";
 import CreateCoupon from "./page/admin/pages/coupon/CreateCoupon";
 import EditCoupon from "./page/admin/pages/coupon/EditCoupon";
 import FindCoupons from "./page/user/profileDashboard/pages/findCoupons";
-// import OrderConfirmation from "./page/user/components/OrderConfirmation";
+import OrderConfirmation from "./page/user/components/OrderConfirmation";
 import SettingsPage from "./page/user/profileDashboard/pages/settings";
 import About from "./page/user/others/About";
 import Home from "./page/user/others/Home";
@@ -172,10 +172,10 @@ function App() {
             element={<ProtectedRoute element={<Checkout />} />}
           />
 
-          {/* <Route
+          <Route
             path="/order-confirmation"
             element={<ProtectedRoute element={<OrderConfirmation />} />}
-          /> */}
+          />
 
           <Route
             path="/buy-now"
