d40782b2f539712470dbec5e709caa4326a3c9cf
shaamilshan <shmlvk123@gmail.com>
2026-01-07 19:44:01 +0530

    2 cards per row for mobile screen

--- client/src/page/user/others/Collection.jsx
+++ client/src/page/user/others/Collection.jsx
@@ -663,7 +663,7 @@ const Collections = () => {
                     <JustLoading size={10} />
                   </div>
                 ) : (
-                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 sm:gap-4 md:gap-5 py-3">
+                  <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 sm:gap-4 md:gap-5 py-3">
                     {userProducts && userProducts.length > 0 ? (
                       userProducts.map((pro, index) => (
                         <ProductCard2
