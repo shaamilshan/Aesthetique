4cf2b62c13f7acbedf9c602b7cadaf52d6492937
shaamilshan <shmlvk123@gmail.com>
2026-01-14 19:15:11 +0530

    admin mobile logo

--- client/src/components/ExIphoneLogo.jsx
+++ client/src/components/ExIphoneLogo.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Logo from "../assets/others/Logo.svg";
+import Logo from "../assets/others/bm-logo.png";
 
 const ExIphoneLogo = () => {
   return (
     <>
-      <img src={Logo} alt="BM. logo" />
+  <img src={Logo} alt="BM Aesthetique" className="h-8 w-auto object-contain" />
       {/* <p>TrendKart</p> */}
     </>
   );
